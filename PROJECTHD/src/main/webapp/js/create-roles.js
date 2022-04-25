205e8b7b24bfecb1c4d21dc9677d14434de24c29
f-ksg <f-ksg@hotmail.com>
2022-04-25 19:47:48 +0800

    Sprint1-Commit-01 25th April 2022

---
+++ PROJECTHD/src/main/webapp/js/create-roles.js
@@ -0,0 +1,12 @@
+function createRoles()
+{
+    var roleName = document.getElementById("roleName").value;
+    window.confirm("Role: " + roleName + " confirm submit?");
+    //add role to database(?)
+    //if success
+    alert("Create Role Successful");
+    //else
+    //alert("Create Roles fail");
+    
+
+}
