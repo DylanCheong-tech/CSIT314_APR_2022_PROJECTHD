45feca0c7b74ab9684f5dd050b6cb3bbb98327ff
Rhynst <winsonhuang81@gmail.com>
2022-04-25 16:32:13 +0800

    Sprint1-Commit-02 25th April 2022
    What’s new:
    None
    
    Database
     None
    
    Backend
    1. Account class has been modified
    2. Role class has been modified
    
    Frontend
    None
    
    Done by : Winson Junior

--- PROJECTHD/src/main/java/Account.java
+++ PROJECTHD/src/main/java/Account.java
@@ -1,4 +1,39 @@
+import restaurant.Account;
+import restaurant.Role;
 
 public class Account {
-	int id
+	private static int tempID = 1;
+	private int accountId;
+	private String username;
+	private String password;
+	private String name;
+	private Role role;
+	private String dateJoined;
+	
+	public Account() {
+		this.accountId = tempID;
+		tempID++;
+	}
+	
+	public Account(String username, String password, String name, Role role, String dateJoined) {
+		this.accountId = tempID;
+		tempID++;
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.role = role;
+		this.dateJoined = dateJoined;
+	}
+	
+	public Account(Account account) {
+		this.accountId = account.accountId;
+		this.username = account.username;
+		this.password = account.password;
+		this.name = account.name;
+		this.role = account.role;
+		this.dateJoined = account.dateJoined;
+	}
+	
+	
+	
 }
--- PROJECTHD/src/main/java/Role.java
+++ PROJECTHD/src/main/java/Role.java
@@ -1,4 +1,27 @@
+import restaurant.Role;
 
 public class Role {
+	private static int tempID = 1;
+	private int roleId;
+	private String name;
+	private String description;
+	
+	public Role() {
+		this.roleId = tempID;
+		tempID++;
+	}
+	
+	public Role(String name, String description) {
+		this.roleId = tempID;
+		tempID++;
+		this.name = name;
+		this.description = description;
+	}
+	
+	public Role(Role role) {
+		this.roleId = role.roleId;
+		this.name = role.name;
+		this.description = role.description;
+	}
 
 }
