656d79e5d6a2d5757a8b6a0dcaa5132e3f6c8a85
f-ksg <f-ksg@hotmail.com>
2022-04-27 03:23:01 +0800

    Sprint1-Commit-13 26th April 2022
    What's new:
    Updated create-roles.html to look like wireframe
    create-roles.css to look like wireframe
    create-roles.js for the time and buttons

--- PROJECTHD/src/main/webapp/js/create-roles.js
+++ PROJECTHD/src/main/webapp/js/create-roles.js
@@ -1,7 +1,7 @@
 function createRoles()
 {
-    var roleName = document.getElementById("roleName").value;
-    window.confirm("Role: " + roleName + " confirm submit?");
+    var roleName = document.getElementById("roleID").value;
+    window.alert("Create Roles Successful");
     //connect to database
     //add role to database
     //if success
@@ -10,4 +10,23 @@ function createRoles()
     //alert("Create Roles fail");
     
 
+}
+
+setInterval(() => 
+{
+    var currentDate = new Date();
+    var date = currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1) + '-' + currentDate.getDate();
+    var time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+
+    document.getElementById("header-date-time").innerHTML = date + "      " + time;
+}, 1000);
+
+function userAdminPortal()
+{
+    window.location.href="./webapp/user-admin-portal.html";
+}
+
+function displayLogout () 
+{
+    window.confirm("Are you sure want to logout ? ");
 }
