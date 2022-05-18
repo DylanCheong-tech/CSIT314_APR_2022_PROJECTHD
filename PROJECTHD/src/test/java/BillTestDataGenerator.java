8b7d0b3c1076c90d11adfa1b23b6f09eb95be999
Rhynst <winsonhuang81@gmail.com>
2022-05-18 13:54:14 +0800

    Sprint4-Commit-88 18th May 2022
    What’s new:
    1. None
    
    Database
    1. None
    
    Backend
    1. BillTestDataGenerator.java has been modified
    
    Frontend
    1. None
    
    Testing
    1. None
    
    Done by: Winson Junior

--- PROJECTHD/src/test/java/BillTestDataGenerator.java
+++ PROJECTHD/src/test/java/BillTestDataGenerator.java
@@ -6,20 +6,15 @@ import honeyzstar.entity.*;
 
 public class BillTestDataGenerator {
 	public static void main(String[] args) {
-		ArrayList<BillStatus> billStatus = new ArrayList<BillStatus>();
-		billStatus.add(BillStatus.Paid);
-		billStatus.add(BillStatus.Unpaid);
 		
 		String email = "This is my email ";
 		String code = "Coupon";
-		int count = 1;
 		
 		for (int i = 0; i < 100; i++) {
 			Random rn = new Random();
-			int randomOrderID = rn.nextInt(100) + 301 ;
+			int randomOrderID = rn.nextInt(100) + 1 ;
 			int randomCouponID = rn.nextInt(100) + 1 ;
 			int randomCode = rn.nextInt(100) + 1 ;
-			int randomBillStatus = rn.nextInt(2);
 			int randomValue = rn.nextInt(2);
 			int randomEmail = rn.nextInt(2);
 			
