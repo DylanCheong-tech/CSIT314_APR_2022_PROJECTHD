0695b3caa061f72ecc77990a093a7ca1b7b5cfbd
Rhynst <winsonhuang81@gmail.com>
2022-05-19 08:59:09 +0800

    Sprint4-Commit-90 19th May 2022
    What’s new:
    1. None
    
    Database
    1. None
    
    Backend
    1.AccountTestDataGenerator.java has been modified
    2. BillTestDataGenerator.java has been modified
    
    Frontend
    1. None
    
    Testing
    1. None
    
    Done by: Winson Junior

--- PROJECTHD/src/test/java/AccountTestDataGenerator.java
+++ PROJECTHD/src/test/java/AccountTestDataGenerator.java
@@ -6,58 +6,6 @@ import honeyzstar.entity.Role;
 
 import java.sql.*;
 
-enum Month {
-    January(1),
-    February(2),
-    March(3),
-    April(4),
-    May(5),
-    June(6),
-    July(7),
-    August(8),
-    September(9),
-    October(10),
-    November(11),
-    December(12);
-
-    private int value;
-
-    Month(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-}
-
-class Date {
-	private final int year;
-    private final Month month;
-    private final int day;
-
-    public Date(int day, Month month, int year){
-    	this.year = year;
-        this.month = month;
-        this.day = day;
-    }
-    
-    public int getYear() {
-    	return year;
-    }
-
-    public Month getMonth(){
-        return month;
-    }
-
-    public int getDay(){
-        return day;
-    }
-
-    public String toString(){
-        return String.format("%d %s %d", day, String.valueOf(month), year);
-    }
-}
 
 public class AccountTestDataGenerator {
 	private static final String connStr = "jdbc:mysql://localhost:3306/csit314_apr_2022_projecthd?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
--- PROJECTHD/src/test/java/BillTestDataGenerator.java
+++ PROJECTHD/src/test/java/BillTestDataGenerator.java
@@ -1,3 +1,12 @@
+import java.lang.System.Logger;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
@@ -5,12 +14,37 @@ import java.util.Random;
 import honeyzstar.entity.*;
 
 public class BillTestDataGenerator {
+	
+	
+	
+	
+	
+	private static long getRandomTimeBetweenTwoDates () {
+		long beginTime = Timestamp.valueOf("2020-01-01 10:00:00").getTime();
+
+		long endTime = Timestamp.valueOf("2022-12-31 20:59:59").getTime();
+
+	    long diff = endTime - beginTime + 1;
+	    return beginTime + (long) (Math.random() * diff);
+	}
+	
 	public static void main(String[] args) {
+		final String connStr = "jdbc:mysql://localhost:3306/csit314_apr_2022_projecthd?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+	    final String dbusername = "root";
+	    final String dbpassword = "";
 		
 		String email = "This is my email ";
 		String code = "Coupon";
 		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		
 		for (int i = 0; i < 100; i++) {
+			Date randomDate = new Date(getRandomTimeBetweenTwoDates());
+			String date = dateFormat.format(randomDate);
+			randomDate.getTime();
+			
+			
+			
 			Random rn = new Random();
 			int randomOrderID = rn.nextInt(100) + 1 ;
 			int randomCouponID = rn.nextInt(100) + 1 ;
@@ -27,9 +61,33 @@ public class BillTestDataGenerator {
 			Bill bill = new Bill();
 			bill.setOrder(order);
 			
-			bill.createBill();
+			
+			try (
+
+	                Connection conn = DriverManager.getConnection(
+	                        connStr, dbusername, dbpassword);
+
+	        ) {
+
+	            PreparedStatement stmt = conn.prepareStatement(
+	                    "select auto_increment from information_schema.tables where table_name = 'Bill'");
+	            ResultSet result = stmt.executeQuery();
+	            if (result.next()) {
+	                bill.setBillID(result.getInt("auto_increment"));
+	            }
+
+	            PreparedStatement stmt1 = conn.prepareStatement("INSERT INTO BILL (OrderID, CreatedAt) VALUES (?, ?)");
+	            stmt1.setInt(1, bill.getOrder().getOrderID());
+	            stmt1.setString(2, date);
+
+	            stmt1.executeUpdate();
+
+	        } catch (SQLException ex) {
+	            ex.printStackTrace();
+	        }
 			
 			
+	
 			
 			if(randomValue == 0) {
 				bill.applyCoupon(code + String.valueOf(randomCode));
@@ -43,7 +101,6 @@ public class BillTestDataGenerator {
 				bill.setEmail(email + String.valueOf(i+1));
 				bill.makePayment(bill.getEmail());
 			}
-
 			
 			
 			
