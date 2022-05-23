38358fd617ed51ec23596c50b4656a528a258027
DylanCheong-tech <74398592+DylanCheong-tech@users.noreply.github.com>
2022-05-23 15:04:23 +0800

    Sprint5-Commit-101 23th May 2022
    What’s new:
    1. Report class is added
    2. Controller classes for Restaurant Owner are added
    3. Frontend UI for Restaurant Owner are added
    
    Database
    1. None
    
    Backend
    1. None
    
    Frontend
    1. Operation Portals header button name ‘Portal Home’ are changed to ‘Home’
    2. Login page design is reviewed and modified
    
    Testing
    1. None
    
    Done by: Cheong Wai Hong

--- PROJECTHD/src/main/webapp/js/login.js
+++ PROJECTHD/src/main/webapp/js/login.js
@@ -21,36 +21,22 @@ function remove_fail_alert(){
     document.getElementById("login-fail-msg").innerHTML = "";
 }
 
-var role_list = $.ajax({
-    async :false,
+$.ajax({
+    async : true,
     "url": "/getRoleList",
     "type": "get",
-    "dataType": "json"
-}).responseJSON;
-
-var select = document.getElementById("role-list");
-for (index in role_list) {
-    var option = document.createElement("option");
-    option.value = role_list[index].roleID;
-    option.innerText = role_list[index].name;
-    select.appendChild(option);
-}
-
-setInterval(() => {
-    var bg_img_frame = document.getElementById("image-frame");
-    var current_left_pos = bg_img_frame.style.left ? bg_img_frame.style.left : "-1%";
-    current_left_pos = parseInt(current_left_pos.substring(0, current_left_pos.length - 1));
-
-    if (current_left_pos > -250){
-        current_left_pos -= 104;
-        bg_img_frame.style.left = current_left_pos + "%";
+    "dataType": "json",
+    "complete" : (data) => {
+        var role_list = data.responseJSON;
+        var select = document.getElementById("role-list");
+        for (index in role_list) {
+            var option = document.createElement("option");
+            option.value = role_list[index].roleID;
+            option.innerText = role_list[index].name;
+            select.appendChild(option);
+        }
     }
-    else{
-        current_left_pos = -1;
-        bg_img_frame.style.left = current_left_pos + "%";
-    }
-    
-}, 4000);
+});
 
 function updateLoginAction () {
     var form = document.getElementById("login-form");
